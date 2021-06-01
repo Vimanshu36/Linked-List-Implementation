8c09d1e70661603c6d65688279958cf79c6a6756
Vimanshu Rajpurohit <vimanshu36@gmail.com>
2021-06-01 10:21:06 +0530

    Update L_40_LinkedListClient.java

--- L_40_LinkedListClient.java
+++ L_40_LinkedListClient.java
@@ -8,17 +8,5 @@ public class L_40_LinkedListClient {
         ll.addLast(40);
         ll.addFirst(5);       
         ll.addAt(3, 66);
-        // ll.removeLast();
-        // ll.removeAt(1);
-        // ll.reverseIteratively();
-        // ll.reverseRecursively();                    
-        // ll.reverseRecursivelyDeep();
-        // // ll.reverseRecursiveOneVar();
-        // System.out.println(ll.midPoint());
-        // System.out.println(ll.kthfromLast(2));
-        // ll.Display();
-
-        ll.createDummyList();
-
     }
 }
